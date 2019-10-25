587d908cb3f7c48f2db1c4c005ceb311ff705d38
Raffles <rafilson@hotmail.com>
2019-10-25 22:47:45 +0800

    Create UI (form) for Create Project Page

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="create-form container fluid">
     <b-form @submit="onSubmit" @reset="onReset">
-      <b-form-group id="input-group-project-title" label="Project Title:" label-for="input-project-title">
+      <b-form-group id="input-group-project-name" label="Project Name:" label-for="input-project-title">
         <b-form-input
                 id="input-project-title"
-                v-model="form.projectTitle"
+                v-model="form.projectName"
                 type="text"
                 required
         ></b-form-input>
@@ -13,7 +13,7 @@
       <b-form-group id="input-group-category" label="Category:" label-for="input-category">
         <b-form-select
                 id="input-category"
-                v-model="form.category"
+                v-model="form.projectCategory"
                 :options="categories"
                 required
         ></b-form-select>
@@ -22,7 +22,7 @@
       <b-form-group id="input-group-image-url" label="Image Url:" label-for="input-image-url">
         <b-form-input
                 id="input-image-url"
-                v-model="form.imageUrl"
+                v-model="form.projectImageUrl"
                 placeholder="E.g. https://unsplash.com/photos/hD5HgJzhjG4"
         ></b-form-input>
       </b-form-group>
@@ -30,7 +30,7 @@
       <b-form-group id="input-group-deadline" label="Project End Date:" label-for="input-deadline">
         <b-form-input
                 id="input-deadline"
-                v-model="form.deadline"
+                v-model="form.projectDeadline"
                 type="date"
                 required
         ></b-form-input>
@@ -39,7 +39,7 @@
       <b-form-group id="input-group-funding-goal" label="Funding Goal: " label-for="input-funding-goal">
         <b-form-input
                 id="input-funding-goal"
-                v-model="form.fundingGoal"
+                v-model="form.projectFundingGoal"
                 type="number"
                 required
         ></b-form-input>
@@ -48,7 +48,7 @@
       <b-form-group id="input-group-description" label="Project Description: " label-for="textarea-description">
         <b-form-textarea
                 id="textarea-description"
-                v-model="form.description"
+                v-model="form.projectDescription"
                 placeholder="Tell us more about your project..."
                 rows="3"
                 max-rows="16"
@@ -70,12 +70,12 @@
     data() {
       return {
         form: {
-          projectTitle: '',
-          category: '',
-          imageUrl: '',
-          deadline: '',
-          fundingGoal: '',
-          description: ''
+          projectName: 'Aer',
+          projectCategory: 'Crafts',
+          projectImageUrl: 'image.com',
+          projectDeadline: "2019-10-15",
+          projectFundingGoal: '45000',
+          projectDescription: 'Good bag that is good'
         },
         categories: ['Arts', 'Crafts', 'Electronics', 'Games']
       }
@@ -84,23 +84,32 @@
       onSubmit(evt) {
         evt.preventDefault()
         axios
-                .post("http://localhost:3030/create", this.form)
-                .then(response => response.json("Success"))
-                .catch(error => alert(error))
-        //alert(JSON.stringify(this.form))
+                .post("http://localhost:3000/create", {
+                  projectName: this.form.projectName,
+                  projectCategory: this.form.projectCategory,
+                  projectImageUrl: this.form.projectImageUrl,
+                  projectDeadline: this.form.projectDeadline,
+                  projectFundingGoal: this.form.projectFundingGoal,
+                  projectDescription: this.form.projectDescription,
+                })
+                .then(
+                        function(response) {
+                          alert("Project Created!");
+                        },
+                        function(response) {
+                          alert("Unable to create project");
+                        }
+                );
       },
       onReset(evt) {
         evt.preventDefault()
         // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
+        this.form.projectName = ''
+        this.form.projectCategory = ''
+        this.form.projectImageUrl = ''
+        this.form.projectDeadline = ''
+        this.form.projectFundingGoal = ''
+        this.form.projectDescription = ''
       }
     }
   }
