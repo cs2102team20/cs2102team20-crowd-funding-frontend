94e7d588f4959c7d8040f45900ef11e6854e3717
Raffles <rafilson@hotmail.com>
2019-10-26 20:53:21 +0800

    Connect project and rewards data (from response data) to view DOM.

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <b-jumbotron header="Project Title" lead="Project Description">
+        <b-jumbotron v-bind:header="project.project_name">
             <!-- Project Image  -->
             <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
                 <b-card-text>
-                    <h1>$50,000 CURRENT FUNDING</h1>
-                    <p>pledged of $25,000 funding goal</p>
+                    <h1>$50,XXX CURRENT FUNDING</h1>
+                    <p>pledged of ${{project.project_funding_goal}} funding goal</p>
                 </b-card-text>
                 <b-card-text>
-                    <h3>3,322</h3>
+                    <h3>3,xxx </h3>
                     <p>backers</p>
                 </b-card-text>
                 <b-card-text>
-                    <h3>13</h3>
+                    <h3>{{this.dayTillDeadline()}}</h3>
                     <p>days to go</p>
                 </b-card-text>
                 <p>
@@ -21,7 +21,8 @@
                 <p>
                     <b-button variant="outline-secondary"><i class="fa fa-heart" aria-hidden="true"></i> Like</b-button>
                 </p>
-                <p>This project will only be funding if it reaches its goal by (PROJECT_DEADLINE)</p>
+                <p>This project will only be funding if it reaches
+                    its goal by {{this.getFormattedDate(new Date(this.project.project_deadline))}}</p>
             </b-card>
         </b-jumbotron>
 
@@ -41,43 +42,22 @@
                 <b-col cols="8">
                     <h2>Project Description</h2>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                        but also the leap into electronic typesetting, remaining essentially unchanged. It was
-                        popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                        and more recently with desktop publishing software like Aldus PageMaker including versions of
-                        Lorem Ipsum.
+                        {{this.project.project_description}}}
                     </p>
                 </b-col>
                 <b-col>
                     <h2>Rewards</h2>
                     <b-card
-                            title="Pledge $40"
+                            :title="'$' + reward.reward_pledge_amount"
                             tag="article"
                             style="max-width: 20rem;"
                             class="mb-2"
+                            v-for="(reward, index) in rewards" :key="index"
                     >
                         <b-card-text>
-                            <h6>Reward Name</h6>
+                            <h6>{{reward.reward_name}}</h6>
                             <p>
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                            </p>
-                        </b-card-text>
-                        <b-button href="#" variant="primary">Pledge</b-button>
-                    </b-card>
-                    <b-card
-                            title="Pledge $40"
-                            tag="article"
-                            style="max-width: 20rem;"
-                            class="mb-2"
-                    >
-                        <b-card-text>
-                            <h6>Reward Name</h6>
-                            <p>
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                {{reward.reward_description}}
                             </p>
                         </b-card-text>
                         <b-button href="#" variant="primary">Pledge</b-button>
@@ -95,23 +75,55 @@
         name: "Project",
         data() {
             return {
-
+                project: null,
+                rewards: null,
             }
         },
         beforeMount() {
             this.loadProject()
+            this.loadRewards()
         },
         methods: {
             loadProject() {
                 axios
                     .get("http://localhost:3000/project/" + this.$route.params.projectName)
                     .then((response) => {
-                        alert(response.data);
+                        console.log(response.data);
+                        this.project = response.data
                     })
                     .catch((error) => {
                         // Failure
                         alert(error);
                     });
+            },
+            loadRewards() {
+                axios
+                    .get("http://localhost:3000/project/" + this.$route.params.projectName + "/rewards")
+                    .then((response) => {
+                        console.log(response.data);
+                        this.rewards = response.data
+                    })
+                    .catch(error => {
+                        alert(error)
+                    })
+            },
+            dayTillDeadline() {
+                const date1 = new Date();
+                const date2 = new Date(this.project.project_deadline);
+                const diffTime = Math.abs(date2 - date1);
+                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                return diffDays;
+            },
+            getFormattedDate(date) {
+                    var year = date.getFullYear();
+
+                    var month = (1 + date.getMonth()).toString();
+                    month = month.length > 1 ? month : '0' + month;
+
+                    var day = date.getDate().toString();
+                    day = day.length > 1 ? day : '0' + day;
+
+                    return year + '/' + month + '/' + day;
             }
         },
     }
