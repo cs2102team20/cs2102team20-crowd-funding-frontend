7e46dbfe797c621aad2efea65251241c4f5bfba5
Raffles <rafilson@hotmail.com>
2019-11-05 10:58:23 +0800

    Link data in ProjectBackedSection for Profile page.

---
+++ src/sub-components/ProjectsBackedSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <b-card-group columns v-for="(project, index) in backedProjects" :key="index">
+            <b-card
+                    title="Card title that wraps to a new line"
+                    img-alt="Image"
+                    img-top
+                    img-src="https://unsplash.com/photos/B1KFwtFFZl8"
+            >
+                <b-card-text>
+                    <h3>{{project.project_name}}</h3>
+                    <p>{{project.project_description}}</p>
+                </b-card-text>
+            </b-card>
+        </b-card-group>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProjectsBackedSection",
+        props: {
+            backedProjects: Array
+        }
+    }
+</script>
+
+<style scoped>
+</style>
