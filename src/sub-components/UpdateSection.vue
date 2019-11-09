6d09f90df6fbcccaf4e6951e0a52bec9eb916503
Raffles <rafilson@hotmail.com>
2019-11-09 13:29:55 +0800

    Fix bug: Update section has no closing div tag.

--- src/sub-components/UpdateSection.vue
+++ src/sub-components/UpdateSection.vue
@@ -42,6 +42,35 @@
         <h4>{{update.update_title}}</h4>
         <p>{{update.update_description}}</p>
       </b-card-text>
+
+      <div class="row bootstrap snippets container main">
+            <div class="col-md-8 col-md-offset-2 col-sm-12">
+                <div class="comment-wrapper">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        Comment panel
+                    </div>
+                    <div class="panel-body">
+                        <div class="clearfix"></div>
+                        <hr>
+                        <ul class="media-list" v-for="(update, index) in updates" :key="index">
+                            <li class="media">
+                                <div class="media-body">
+                                <span class="text-muted pull-right">
+                                    <small class="text-muted">30 min ago</small>
+                                </span>
+                                    <strong class="text-success">@{{comment.email}}</strong>
+                                    <p>
+                                        {{comment.comment_text}}
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+      </div>
     </b-card>
   </b-container>
 </template>
